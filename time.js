693f929f9bb71d03f5ac80796d0d7118b79a05b2
xP4trXxXbot <95147682+xP4trXxXbot@users.noreply.github.com>
2022-02-06 22:38:46 +0100

    Add files via upload

---
+++ time.js
@@ -0,0 +1,37 @@
+const AXE = document.querySelector(".axe");
+const CZAS = document.querySelector(".czas");
+let licznik = 0;
+
+AXE.addEventListener('click', axe = () => {
+    if (coinsLogic >= 2000) {
+        coinsLogic = coinsLogic - 2000;
+        COINS.innerHTML = coinsLogic;
+
+        const stoper = () => {
+            licznik++;
+            CZAS.innerHTML = licznik;
+            for (let i = 0; i < 80; i++) {
+                pointsLogic++;
+            }
+            POINTS.innerHTML = pointsLogic;
+            if(pointsLogic % 20 == 0 || pointsLogic % 4 == 0 || pointsLogic % 7 == 0) {
+                for (let i = 0; i < 20; i++) {
+                    coinsLogic++;
+                }
+                COINS.innerHTML = coinsLogic;
+            }
+            if (licznik === 60) {
+                clearInterval(Interval);
+            }
+            if (licznik === 120) {
+                clearInterval(Interval);
+            }
+            if (licznik === 180) {
+                clearInterval(Interval);
+                licznik = 0;
+            }
+        };
+
+        const Interval = setInterval(stoper, 1000);
+    }
+});
